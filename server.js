3418ce726b7d2741eb8497140cc981dc351eb898
Ahmed Faraz <ahmedfaraz.business@gmail.com>
2022-01-07 20:48:57 +0500

    Added Auth and Users APIs
    Added Auth and Users APIs

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json({ extended: false }));
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "This is Axiom Ecommerce Platform." });
+});
+
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/users", require("./routes/users"));
+
+app.listen(PORT, () => {
+  console.log(`Server has been started\nhttp://localhost:${PORT}`);
+});
